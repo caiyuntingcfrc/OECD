0d58c71f2a9c159296e994eef3d8f5a324bc0f87
CAI, YUN-TING <caiyuntingcfrc@gmail.com>
2019-01-09 17:59:07 +0800

    0109

--- OECD_charts.R
+++ OECD_charts.R
@@ -1,50 +1,68 @@
 library(readxl)
 library(ggplot2)
 library(extrafont)
+library(tidyverse)
+library(reshape2)
 loadfonts(device = "win")
 
 rm(list = ls())
 
+#read and manipulate date
 xls <- "C:/Users/user/Desktop/¡¯SF_2_1_Fertility_rates_LUNG.xlsx"
-df <- as.data.frame(read_xlsx(xls, sheet = 1, range = "L5:R42", col_names = TRUE))
-#colnames(df)
-#cols <- c("country", "replacement rate", "1970", "1995", "2016", "OECD")
-#df[ , cols] <- apply(df[ , cols], 2, as.factor)
-#class(df$country)
-#df <- fortify(df[order(df$"2016"), ])
+df <- read_xlsx(xls, sheet = 1, range = "L5:Q42", col_names = TRUE)
+head(df)
+colnames(df) <- c("country", "NA", "replacement rate", "1970", "1995", "2016")
+colnames(df)
+
+#new variable
+df <- df %>% mutate(
+    member = case_when(
+        country == "Taiwan" ~ "Taiwan", 
+        country == "OECD average" ~ "A", 
+        TRUE ~ "OECD"
+        )
+)
+
 
 #axis color condition
 axiscolor <- ifelse(df$country == "Taiwan", "tan4", ifelse(df$country == "OECD average", "springgreen4", "black"))
 
+#gg
 gg <- ggplot(data = df, aes(x = reorder(`country`, `2016`)))
 
 #barplot
-bar <- geom_bar(aes(y = `2016`, fill = `OECD`, colour = "c1"), stat = "identity", width = 0.5)
+bar <- geom_bar(aes(y = `2016`, fill = `member`), colour = "snow1", stat = "identity", width = 0.5)
 
-#hline replacement rate
-#hline <- geom_hline(aes(color = "red", yintercept = 2.1), size = 0.5, linetype = "longdash", show.legend = FALSE)
-
-#point 1995
-point1 <- geom_point(aes(y = `1995`, fill = "f2", colour = "c2", shape ="s1"), size = 2, show.legend = TRUE)
-#point 1997
-point2 <- geom_point(aes(y = `1970`, fill = "f3", colour = "c3", shape = "s2"), size = 2, show.legend = TRUE)
-#point replacement rate
-point3 <- geom_point(aes(y = `replacement rate`, fill = "f4", colour = "c4", shape = "s3"), size = 2, show.legend = TRUE)
+#points
+c <- melt(df[ , c("country", "1970", "1995", "replacement rate")], id.vars = "country")
+point <- geom_point(data = c, aes(x = `country`, y = `value`, fill = `variable`, colour = "snow1", 
+                                  shape = `variable`), size = 3)
 
 #custom fill and color
-fill <- scale_fill_manual("", 
-                          labels = c("Taiwan" = "Taiwan", "A" = "OECD-average", "T" = "OECD", "f2" = "1995", "f3" = "1997", "f4" = "replacement rate"), 
-                          values = c("Taiwan" = "tan1", "A" = "springgreen3", "T" = "turquoise2", "f2" = "whitesmoke", "f3" = "black", "f4" = "red"),
-                          guide = "legend")
+fill <- scale_fill_manual("A"
+                          labels = c("a", "b", "c"), 
+                          #labels = c("f2" = "1995", "f3" = "1997", "f4" = "replacement rate"), 
+                          values = c("Taiwan" = "tan1", "A" = "springgreen3", "OECD" = "turquoise2", 
+                                     "1970" = 23, "1995" = 23, "replacement rate" = 1),
+                          breaks = c("A", "OECD", "Taiwan")
+                          )
 
-colour <- scale_colour_manual(values = c("c1" = "snow4", "c2" = "snow4", "c3" = "snow4", "c4" = "red"), guide = FALSE)
+colour <- scale_colour_manual("B", 
+                              labels = c("a", "b", "c"), 
+                              #labels = c("Taiwan" = "Taiwan", "A" = "OECD-average", "T" = "OECD", "c2" = "1995", "c3" = "1997", "c4" = "replacement rate"), 
+                              guide = FALSE)
 
-shape <- scale_shape_manual(values = c("s1" = 23, "s2" = 23, "s3" = 3), guide = "legend")
+shape <- scale_shape_manual("C", 
+                            labels = c("a", "b", "c"), 
+                            #labels = c("s2" = "1995", "s3" = "1997", "s4" = "replacement rate"), 
+                            values = c(23, 23, 3)
+                            )
     
 #labels
 lab <- labs(title = "Chart SF2.1.A. Total fertility rate, 1970, 1995 and 2016 or latest available", 
             subtitle = "Average number of children born per woman over a lifetime given current age-specific fertility rates and assuming no female mortality during reproductive years", 
-            size = 12)
+            size = 12
+            )
     
 #customization
 theme <- theme(axis.text.x = element_text(family = "mono", angle = 45, hjust = 1, vjust = 1, colour = axiscolor), 
@@ -64,7 +82,8 @@ theme <- theme(axis.text.x = element_text(family = "mono", angle = 45, hjust = 1
 #axis y
 axis.y <- scale_y_continuous(expand = c(0, 0), limits = c(0, 6))
 
-p <- gg + bar + point1 + point2 + point3 + fill + colour + shape + lab + theme + axis.y #+ guides(guide_legend(override.aes = list(shape = NA)))
+p <- gg + bar + point + fill + shape +lab + theme + axis.y
+
 p
 
 #export the plot
