60f63cc036e260afe4bdd072e40b0574a4b535d6
CAI, YUN-TING <46313402+caiyuntingcfrc@users.noreply.github.com>
2019-01-28 10:26:50 +0800

    Add files via upload

--- OECD_charts.R
+++ OECD_charts.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(extrafont)
 library(tidyverse)
 library(reshape2)
-loadfonts(device = "win")
+loadfonts(device = "pdf")
 
 rm(list = ls())
 
@@ -13,22 +13,37 @@ vignette("ggplot2-specs")
 ####################
 
 #read and manipulate data
-xls <- "C:/Users/user//Desktop/¡¯SF_2_1_Fertility_rates_LUNG.xlsx"
-df <- read_xlsx(xls, sheet = 1, range = "L5:Q42", col_names = TRUE)
+xls <- "~/Downloads/¡¯SF_2_1_Fertility_rates_LUNG.xlsx"
+df <- read_xlsx(xls, sheet = 1, range = "L5:Q62", col_names = TRUE)
 head(df)
 colnames(df) <- c("country", "NA", "replacement rate", "1970", "1995", "2016")
 colnames(df)
 
-#new variable
+#remove empty columns
+df <- df[rowSums(is.na(df)) != ncol(df), ]
+
+#new variable `member`
 df <- df %>% mutate(
     member = case_when(
         country == "Taiwan" ~ "Taiwan", 
         country == "OECD average" ~ "A", 
+        row.names(df) %in% 38:56 ~ "non-OECD", 
         TRUE ~ "OECD"
-        )
+        ), 
+    group = case_when(
+        member == "Taiwan" ~ 0, 
+        member == "A" ~ 0,
+        member == "OECD" ~ 0,
+        TRUE ~ 1
+    )
 )
 
-df$member <- factor(df$member)
+#factorize
+df$country <- factor(df$country, levels = df$country)
+#df$member <- factor(df$member)
+df$group <- factor(df$group, levels = c(0, 1))
+#sort df
+#df <- df[with(df, order(`group`, `2016`)), ]
 
 #axis color condition
 axiscolor <- ifelse(df$country == "Taiwan", "tan4", 
@@ -36,23 +51,24 @@ axiscolor <- ifelse(df$country == "Taiwan", "tan4",
 #Plot
 p <- ggplot() +
     #barplot
-    geom_bar(data = df, aes(x = reorder(`country`, `2016`), y = `2016`, fill = `member`), 
-             colour = "grey", stat = "identity", width = 0.7) + 
+    geom_bar(data = df, aes(x = df$country, y = `2016`, fill = `member`), 
+             colour = "grey", stat = "identity", width = 0.7, position = position_dodge()) + 
     #points
     geom_point(data = melt(df[ , c("country", "1970", "1995")], id.vars = "country"), 
                aes(x = `country`, y = `value`, colour = `variable`, fill = `variable`, 
-                   shape = `variable`), size = 3, inherit.aes = FALSE) + 
-    #geom_point(data = c, aes(x = `country`, y = `value`, shape = `variable`), inherit.aes = FALSE)
-    #line
+                   shape = `variable`), size = 3) + 
+    #line(fertility replacement rate)
     geom_line(data = df, aes(x = `country`, y = 2.1, linetype = "solid"), 
               colour = "red", group = 1, size = 1) + 
     #custom fill and color
-    scale_fill_manual("", labels = c("Taiwan" = "Taiwan 2016 ¡ù", "A" = "OECD average", 
-                                          "OECD" = "OECD 2016 ¡ù"), 
+    scale_fill_manual("", labels = c("Taiwan" = "Taiwan 2016", "A" = "OECD average", 
+                                     "OECD" = "OECD", "non-OECD" = "non-OECD"), 
+                      
                       #labels = c("f2" = "1995", "f3" = "1997", "f4" = "replacement rate"), 
                       values = c("Taiwan" = "tan1", "A" = "springgreen3", 
-                                 "OECD" = "turquoise2", "1970" = "black", "1995" = "snow1"), 
-                      breaks = c("Taiwan", "A", "OECD")
+                                 "OECD" = "turquoise2", "non-OECD" = "grey" 
+                                 , "1970" = "black", "1995" = "snow1"), 
+                      breaks = c("Taiwan", "A", "OECD", "non-OECD")
                       ) + 
     scale_colour_manual("child per woman", labels = c("1970" = "1970", "1995" = "1995"),
                         values = c("1970" = "black", "1995" = "black")
@@ -62,10 +78,11 @@ p <- ggplot() +
                        values = c("1970" = 23, "1995" = 23)
                        ) + 
     scale_linetype_manual("", 
-                          labels = c("populationi replacement rate"), 
+                          labels = c("population replacement rate"), 
                           values = c("solid")) +
-    guides(fill = guide_legend(override.aes = list(shape = NA)),
-           colour = guide_legend(override.aes = list(fill = c("black", "snow1")))
+    guides(fill = guide_legend(override.aes = list(shape = NA), order = 2),
+           colour = guide_legend(override.aes = list(fill = c("black", "snow1")), order = 1),
+           shape = guide_legend(order = 1)
            ) +
     #labels
     labs(title = "Chart SF2.1.A. Total fertility rate, 1970, 1995 and 2016 or latest available", 
@@ -82,7 +99,7 @@ p <- ggplot() +
           axis.ticks.y = element_line(colour = "snow4", size = 1, linetype = "solid"), 
           axis.ticks.length = unit(0.3, "cm"), 
           axis.ticks.x = element_blank(), 
-          panel.grid.minor = element_line(size = 0.1, colour = alpha("grey28", 0.1)), 
+          panel.grid.minor = element_line(size = 0.1, colour = alpha("grey28", 0.5)), 
           #panel.grid.major= element_blank(), 
           #panel.grid = element_blank(), 
           panel.background = element_blank(), 
@@ -92,4 +109,4 @@ p <- ggplot() +
     #axis y
     scale_y_continuous(expand = c(0, 0), limits = c(0, 6)) ; p
 #export the plot
-ggsave(filename = "C:/Users/user/Desktop/tst2_plot.png", width = 8.68, height = 5.89, dpi = 600)
+ggsave(filename = "~/Desktop/tst2_plot.png", width = 16, height = 9, dpi = 600)
